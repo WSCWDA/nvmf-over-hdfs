2743ef28e87040b35d3afcde39aa9f419dbf434d
“cwd” <347895316@qq.com>
2023-12-22 16:11:18 +0800

    add lisafuse

---
+++ lisafuse/CMakeFiles/lfs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lfs.dir/rewrite.c.o"
+  "lfs.pdb"
+  "lfs"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/lfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
